c6992c6775fa1d0d6b3af3d1cab5aea4e1e30097
itsOnly1Jah <kingjah23@gmail.com>
2024-04-09 11:19:01 -0400

    feat: replace references of tab with component

--- app/(dashboard)/property/[id]/rental-info/page.tsx
+++ app/(dashboard)/property/[id]/rental-info/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import Sidebar from "@/components/Sidebar"
-import PropertyCard from "@/components/PropertyCard"
-import RentalInfo from "@/components/PropertyRentalInfo"
+import PropertyTab from "@/components/PropertyTab"
 
 import Image from "next/image"
 import Link from "next/link"
@@ -40,13 +39,6 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
-
 
 const Property = ({ params }) => {
   return (
@@ -162,61 +154,7 @@ const Property = ({ params }) => {
           </DropdownMenu>
         </header>
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          <Tabs defaultValue="all">
-            <div className="flex items-center">
-              <TabsList>
-                <TabsTrigger value="all">All</TabsTrigger>
-                <TabsTrigger value="active">Active</TabsTrigger>
-                <TabsTrigger value="draft">Draft</TabsTrigger>
-                <TabsTrigger value="archived" className="hidden sm:flex">
-                  Archived
-                </TabsTrigger>
-              </TabsList>
-              <div className="ml-auto flex items-center gap-2">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="sm" className="h-8 gap-1">
-                      <ListFilter className="h-3.5 w-3.5" />
-                      <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                        Filter
-                      </span>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuCheckboxItem checked>
-                      Active
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                      Archived
-                    </DropdownMenuCheckboxItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-                <Button size="sm" variant="outline" className="h-8 gap-1">
-                  <File className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Export
-                  </span>
-                </Button>
-                <Button size="sm" className="h-8 gap-1">
-                  <PlusCircle className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Add Product
-                  </span>
-                </Button>
-              </div>
-            </div>
-            <TabsContent value="all">
-              <PropertyCard 
-                dashboard = "dashboard-06-chunk-0"
-                title="Property Evaluator"
-                description="Manage your properties and view their projected performance."
-                content=<RentalInfo id={params.id}/>
-              />
-            </TabsContent>
-          </Tabs>
+          <PropertyTab id={params.id} defaultTab="rental"/>
         </main>
       </div>
     </div>
--- app/(dashboard)/property/[id]/report/page.tsx
+++ app/(dashboard)/property/[id]/report/page.tsx
@@ -3,6 +3,7 @@
 import Sidebar from "@/components/Sidebar"
 import PropertyCard from "@/components/PropertyCard"
 import PropertyReport from "@/components/PropertyReport"
+import PropertyTab from "@/components/PropertyTab"
 
 import Image from "next/image"
 import Link from "next/link"
@@ -162,61 +163,7 @@ const Property = ({ params }) => {
           </DropdownMenu>
         </header>
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          <Tabs defaultValue="all">
-            <div className="flex items-center">
-              <TabsList>
-                <TabsTrigger value="all">All</TabsTrigger>
-                <TabsTrigger value="active">Active</TabsTrigger>
-                <TabsTrigger value="draft">Draft</TabsTrigger>
-                <TabsTrigger value="archived" className="hidden sm:flex">
-                  Archived
-                </TabsTrigger>
-              </TabsList>
-              <div className="ml-auto flex items-center gap-2">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="sm" className="h-8 gap-1">
-                      <ListFilter className="h-3.5 w-3.5" />
-                      <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                        Filter
-                      </span>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuCheckboxItem checked>
-                      Active
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                      Archived
-                    </DropdownMenuCheckboxItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-                <Button size="sm" variant="outline" className="h-8 gap-1">
-                  <File className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Export
-                  </span>
-                </Button>
-                <Button size="sm" className="h-8 gap-1">
-                  <PlusCircle className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Add Product
-                  </span>
-                </Button>
-              </div>
-            </div>
-            <TabsContent value="all">
-              <PropertyCard 
-                dashboard = "dashboard-06-chunk-0"
-                title="Property Evaluator"
-                description="Manage your properties and view their projected performance."
-                content=<PropertyReport id={params.id}/>
-              />
-            </TabsContent>
-          </Tabs>
+          <PropertyTab id={params.id} defaultTab="report"/>
         </main>
       </div>
     </div>
