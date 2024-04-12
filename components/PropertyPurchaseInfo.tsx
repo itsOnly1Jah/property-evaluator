d67ac217f7ebf78c52688ccb2513f9c3a7eb59a3
itsOnly1Jah <kingjah23@gmail.com>
2024-04-12 13:48:46 -0400

    feat: have form post form data to api

--- components/PropertyPurchaseInfo.tsx
+++ components/PropertyPurchaseInfo.tsx
@@ -1,4 +1,5 @@
 import { Property } from '@/types'
+import { FormEvent } from 'react'
 import useSWR from 'swr'
 
 import { Input } from "@/components/ui/input"
@@ -19,6 +20,41 @@ import { Textarea } from "@/components/ui/textarea"
 
 
 const PurchaseInfo = ({ id }) => {
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const json = JSON.stringify({
+      "PurchaseInfo": {
+        "PurchasePrice": Number(formData.get("purchasePrice")),
+        "AfterRepairValue": Number(formData.get("afterRepairValue")),
+        "ClosingCost": Number(formData.get("closingCost")),
+        "EstimatedRepairCost": Number(formData.get("estimatedRepairCost")),
+        "LoanDetails": {
+          "PercentDown": Number(formData.get("percentDown")),
+          "DownPayment": Number(formData.get("downPayment")),
+          "InterestRate": Number(formData.get("interestRate")),
+          "PointsFromLender": Number(formData.get("pointsFromLender")),
+          "OtherFeesFromLender": Number(formData.get("otherFeesFromLender")),
+          "FeesInLoan": Boolean(formData.get("feesInLoan")),
+          "InterestOnly": Boolean(formData.get("interestOnly")),
+          "YearsAmortized": Number(formData.get("yearsAmortized")),
+          "CapRate": Number(formData.get("capRate"))
+        }
+      }
+    })
+
+    await fetch(`http://localhost:9080/api/v1/properties/${id}`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: json
+    }).then(res => console.log(res)).catch(err => console.log(err))
+
+  }
+
   const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then(res => res.json())
   const { data, error } = useSWR(`http://localhost:9080/api/v1/properties?_id=${id}`, fetcher)
 
@@ -26,26 +62,26 @@ const PurchaseInfo = ({ id }) => {
   if (!data) return <div>Loading...</div>
 
   return (
-    <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
+    <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0" onSubmit={onSubmit}>
       <fieldset className="grid gap-6 rounded-lg border p-4">
         <legend className="-ml-1 px-1 text-sm font-medium">
           Info
         </legend>
         <div className="grid gap-3">
           <Label htmlFor="purchasePrice">Purchase Price</Label>
-          <Input id="purchasePrice" type="number" placeholder={data[0].PurchaseInfo.PurchasePrice} />
+          <Input name="purchasePrice" type="number" step='.01' placeholder={data[0].PurchaseInfo.PurchasePrice} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="afterRepairValue-">After Repair Value</Label>
-          <Input id="afterRepairValue" type="number" placeholder={data[0].PurchaseInfo.AfterRepairValue} />
+          <Input name="afterRepairValue" type="number" step='.01' placeholder={data[0].PurchaseInfo.AfterRepairValue} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="closingCost">Closing Cost</Label>
-          <Input id="closingCost" type="number" placeholder={data[0].PurchaseInfo.ClosingCost} />
+          <Input name="closingCost" type="number" step='.01' placeholder={data[0].PurchaseInfo.ClosingCost} />
         </div>
         <div className="grid gap-3">
-          <Label htmlFor="estimatedRepairCost">Closing Cost</Label>
-          <Input id="estimatedRepairCost" type="number" placeholder={data[0].PurchaseInfo.EstimatedRepairCost} />
+          <Label htmlFor="estimatedRepairCost">Estimated Repair Cost</Label>
+          <Input name="estimatedRepairCost" type="number" step='.01' placeholder={data[0].PurchaseInfo.EstimatedRepairCost} />
         </div>
       </fieldset>
       <fieldset className="grid gap-6 rounded-lg border p-4">
@@ -54,7 +90,7 @@ const PurchaseInfo = ({ id }) => {
         </legend>
         <div className="grid gap-3">
           <Label htmlFor="role">Down Payment</Label>
-          <Select defaultValue={data[0].PurchaseInfo.LoanDetails.PercentDown}>
+          <Select name="percentDown">
             <SelectTrigger>
               <SelectValue placeholder={data[0].PurchaseInfo.LoanDetails.PercentDown} />
             </SelectTrigger>
@@ -71,20 +107,24 @@ const PurchaseInfo = ({ id }) => {
               <SelectItem value='0.45'>45%</SelectItem>
               <SelectItem value='0.50'>50%</SelectItem>
             </SelectContent>
-            <Input id="downPayment" type="number" placeholder={data[0].PurchaseInfo.LoanDetails.DownPayment} />
+            <Input name="downPayment" type="number" step='.01' placeholder={data[0].PurchaseInfo.LoanDetails.DownPayment} />
           </Select>
         </div>
         <div className="grid gap-3">
+          <Label htmlFor="interestRate">Interest Rate</Label>
+          <Input name="interestRate" type="number" step='.001' placeholder={data[0].PurchaseInfo.LoanDetails.InterestRate} />
+        </div>
+        <div className="grid gap-3">
           <Label htmlFor="pointsFromLender">Points</Label>
-          <Input id="pointsFromLender" type="number" placeholder={data[0].PurchaseInfo.LoanDetails.PointsFromLender} />
+          <Input name="pointsFromLender" type="number" step='.001' placeholder={data[0].PurchaseInfo.LoanDetails.PointsFromLender} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="otherFeesFromLender">Other</Label>
-          <Input id="otherFeesFromLender" type="number" placeholder={data[0].PurchaseInfo.LoanDetails.OtherFeesFromLender} />
+          <Input name="otherFeesFromLender" type="number" step='.01' placeholder={data[0].PurchaseInfo.LoanDetails.OtherFeesFromLender} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="feesInLoan">Loan Fees & Points</Label>
-          <RadioGroup defaultValue="false">
+          <RadioGroup name="feesInLoan" defaultValue="false">
             <div className="flex items-center space-x-2">
               <RadioGroupItem value="true" id="feesInLoanTrue" />
               <Label htmlFor="feesInLoanTrue">Wrap all loan fees into the loan</Label>
@@ -97,7 +137,7 @@ const PurchaseInfo = ({ id }) => {
         </div>
         <div className="grid gap-3">
           <Label htmlFor="feesInLoan">Interest Only</Label>
-          <RadioGroup defaultValue="false">
+          <RadioGroup name="interestOnly" defaultValue="false">
             <div className="flex items-center space-x-2">
               <RadioGroupItem value="true" id="interestOnlyTrue" />
               <Label htmlFor="interestOnlyTrue">Yes</Label>
@@ -111,11 +151,11 @@ const PurchaseInfo = ({ id }) => {
 
         <div className="grid gap-3">
           <Label htmlFor="yearsAmortized">Years Amortized</Label>
-          <Input id="yearsAmortized" type="number" placeholder={data[0].PurchaseInfo.LoanDetails.YearsAmortized} />
+          <Input name="yearsAmortized" type="number" placeholder={data[0].PurchaseInfo.LoanDetails.YearsAmortized} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="CapRate">Cap Rate</Label>
-          <Input id="capRate" type="number" placeholder={data[0].PurchaseInfo.LoanDetails.CapRate} />
+          <Input name="capRate" type="number" step='.01' placeholder={data[0].PurchaseInfo.LoanDetails.CapRate} />
         </div>
       </fieldset>
       <fieldset className="grid gap-6 rounded-lg border p-4">
@@ -124,7 +164,7 @@ const PurchaseInfo = ({ id }) => {
         </legend>
         <div className="grid gap-3">
           <Label htmlFor="content">Content</Label>
-          <Textarea id="content" placeholder="You are a..." />
+          <Textarea name="content" placeholder="You are a..." />
         </div>
       </fieldset>
       <Button type="submit">Save</Button>
--- components/PropertyRentalInfo.tsx
+++ components/PropertyRentalInfo.tsx
@@ -1,4 +1,5 @@
 import { Property } from '@/types'
+import { FormEvent } from 'react'
 import useSWR from 'swr'
 
 import { Input } from "@/components/ui/input"
@@ -6,6 +7,53 @@ import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 
 const RentalInfo = ({ id }) => {
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const json = JSON.stringify({
+      "RentalInfo": {
+        "Income": {
+          "TotalGrossMonthlyRent": Number(formData.get("totalGrossMonthlyRent")),
+          "OtherMonthlyIncome": Number(formData.get("otherMonthlyIncome"))
+        },
+        "FixedExpenses": {
+          "Electricity": Number(formData.get("electricity")),
+          "WaterSewer": Number(formData.get("waterSewer")),
+          "Pmi": Number(formData.get("pmiPercentage")),
+          "Garbage": Number(formData.get("garbage")),
+          "Hoa": Number(formData.get("hoa")),
+          "MonthlyInsurance": Number(formData.get("monthlyInsurance")),
+          "PropertyTaxes": Number(formData.get("propertyTaxes")),
+          "OtherMonthlyExpenses": Number(formData.get("otherMonthlyExpenses"))
+        },
+        "VariableExpenses": {
+          "Vacancy": Number(formData.get("vacancy")),
+          "RepairsMaintenance": Number(formData.get("repairsMaintenance")),
+          "CapitalExpenditures": Number(formData.get("capitalExpenditures")),
+          "ManagementFees": Number(formData.get("managementFees")),
+        },
+        "FutureAssumptions": {
+          "IncomeGrowth": Number(formData.get("incomeGrowth")),
+          "PvGrowth": Number(formData.get("pvGrowth")),
+          "ExpenseGrowth": Number(formData.get("expenseGrowth")),
+          "SaleExpenses": Number(formData.get("saleExpenses")),
+        }
+      }
+    })
+
+    await fetch(`http://localhost:9080/api/v1/properties/${id}`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: json
+    }).then(res => console.log(res)).catch(err => console.log(err))
+
+  }
+
   const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then(res => res.json())
   const { data, error } = useSWR(`http://localhost:9080/api/v1/properties?_id=${id}`, fetcher)
 
@@ -13,18 +61,18 @@ const RentalInfo = ({ id }) => {
   if (!data) return <div>Loading...</div>
 
   return (
-    <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
+    <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0" onSubmit={onSubmit}>
       <fieldset className="grid gap-6 rounded-lg border p-4">
         <legend className="-ml-1 px-1 text-sm font-medium">
           Income
         </legend>
         <div className="grid gap-3">
           <Label htmlFor="totalGrossMonthlyRent">Monthly Rent</Label>
-          <Input id="totalGrossMonthlyRent" type="number" placeholder={data[0].RentalInfo.Income.TotalGrossMonthlyRent} />
+          <Input name="totalGrossMonthlyRent" type="number" placeholder={data[0].RentalInfo.Income.TotalGrossMonthlyRent} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="otherMonthlyIncome">Other Income</Label>
-          <Input id="otherMonthlyIncome" type="number" placeholder={data[0].RentalInfo.Income.OtherMonthlyIncome} />
+          <Input name="otherMonthlyIncome" type="number" placeholder={data[0].RentalInfo.Income.OtherMonthlyIncome} />
         </div>
       </fieldset>
       <fieldset className="grid gap-6 rounded-lg border p-4">
@@ -33,35 +81,35 @@ const RentalInfo = ({ id }) => {
         </legend>
         <div className="grid gap-3">
           <Label htmlFor="role">Electricity</Label>
-          <Input id="electricity" type="number" placeholder={data[0].RentalInfo.FixedExpenses.Electricity} />
+          <Input name="electricity" type="number" placeholder={data[0].RentalInfo.FixedExpenses.Electricity} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="waterSewer">Water & Sewer</Label>
-          <Input id="waterSewer" type="number" placeholder={data[0].RentalInfo.FixedExpenses.WaterSewer} />
+          <Input name="waterSewer" type="number" placeholder={data[0].RentalInfo.FixedExpenses.WaterSewer} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="pmiPercentage">PMI</Label>
-          <Input id="pmiPercentage" type="number" placeholder={data[0].RentalInfo.FixedExpenses.PmiPercentage} />
+          <Input name="pmiPercentage" type="number" placeholder={data[0].RentalInfo.FixedExpenses.PmiPercentage} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="garbage">Garbage</Label>
-          <Input id="garbage" type="number" placeholder={data[0].RentalInfo.FixedExpenses.Garbage} />
+          <Input name="garbage" type="number" placeholder={data[0].RentalInfo.FixedExpenses.Garbage} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="hoa">HOA</Label>
-          <Input id="hoa" type="number" placeholder={data[0].RentalInfo.FixedExpenses.HOA} />
+          <Input name="hoa" type="number" placeholder={data[0].RentalInfo.FixedExpenses.HOA} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="monthlyInsurance">Monthly Insurance</Label>
-          <Input id="monthlyInsurance" type="number" placeholder={data[0].RentalInfo.FixedExpenses.MonthlyInsurance} />
+          <Input name="monthlyInsurance" type="number" placeholder={data[0].RentalInfo.FixedExpenses.MonthlyInsurance} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="propertyTaxes">Property Taxes</Label>
-          <Input id="propertyTaxes" type="number" placeholder={data[0].RentalInfo.FixedExpenses.PropertyTaxes} />
+          <Input name="propertyTaxes" type="number" placeholder={data[0].RentalInfo.FixedExpenses.PropertyTaxes} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="otherMonthlyExpenses">Other Expenses</Label>
-          <Input id="otherMonthlyExpenses" type="number" placeholder={data[0].RentalInfo.FixedExpenses.OtherMonthlyExpenses} />
+          <Input name="otherMonthlyExpenses" type="number" placeholder={data[0].RentalInfo.FixedExpenses.OtherMonthlyExpenses} />
         </div>
       </fieldset>
       <fieldset className="grid gap-6 rounded-lg border p-4">
@@ -70,19 +118,19 @@ const RentalInfo = ({ id }) => {
         </legend>
         <div className="grid gap-3">
           <Label htmlFor="vacancy">Vacancy</Label>
-          <Input id="vacancy" type="number" placeholder={data[0].RentalInfo.VariableExpenses.Vacancy} />
+          <Input name="vacancy" type="number" placeholder={data[0].RentalInfo.VariableExpenses.Vacancy} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="repairsMaintenance">Repairs & Maintenance</Label>
-          <Input id="repairsMaintenance" type="number" placeholder={data[0].RentalInfo.VariableExpenses.RepairsMaintenance} />
+          <Input name="repairsMaintenance" type="number" placeholder={data[0].RentalInfo.VariableExpenses.RepairsMaintenance} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="capitalExpenditures">Capital Expenditures</Label>
-          <Input id="capitalExpenditures" type="number" placeholder={data[0].RentalInfo.VariableExpenses.CapitalExpenditures} />
+          <Input name="capitalExpenditures" type="number" placeholder={data[0].RentalInfo.VariableExpenses.CapitalExpenditures} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="managementFees">Management Fees</Label>
-          <Input id="managementFees" type="number" placeholder={data[0].RentalInfo.VariableExpenses.ManagementFees} />
+          <Input name="managementFees" type="number" placeholder={data[0].RentalInfo.VariableExpenses.ManagementFees} />
         </div>
       </fieldset>
       <fieldset className="grid gap-6 rounded-lg border p-4">
@@ -91,19 +139,19 @@ const RentalInfo = ({ id }) => {
         </legend>
         <div className="grid gap-3">
           <Label htmlFor="incomeGrowth">Income</Label>
-          <Input id="incomeGrowth" type="number" placeholder={data[0].RentalInfo.FutureAssumptions.IncomeGrowth} />
+          <Input name="incomeGrowth" type="number" placeholder={data[0].RentalInfo.FutureAssumptions.IncomeGrowth} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="pvGrowth">Property Value</Label>
-          <Input id="pvGrowth" type="number" placeholder={data[0].RentalInfo.FutureAssumptions.PvGrowth} />
+          <Input name="pvGrowth" type="number" placeholder={data[0].RentalInfo.FutureAssumptions.PvGrowth} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="expenseGrowth">Expenses</Label>
-          <Input id="expenseGrowth" type="number" placeholder={data[0].RentalInfo.FutureAssumptions.ExpenseGrowth} />
+          <Input name="expenseGrowth" type="number" placeholder={data[0].RentalInfo.FutureAssumptions.ExpenseGrowth} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="saleExpenses">Sale</Label>
-          <Input id="saleExpenses" type="number" placeholder={data[0].RentalInfo.FutureAssumptions.SaleExpenses} />
+          <Input name="saleExpenses" type="number" placeholder={data[0].RentalInfo.FutureAssumptions.SaleExpenses} />
         </div>
       </fieldset>
       <Button type="submit">Save</Button>
